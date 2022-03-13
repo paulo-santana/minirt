01d07d2f1b7197f0ba50df44f449c79d8c36f927
Paulo Santana <paulo.santana.r@gmail.com>
2022-03-13 12:40:18 -0300

    test: display the dots in a clock

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include "structures.h"
 #include "tuple/tuple.h"
 #include "debug.h"
+#include <math.h>
 #include <mlx.h>
 
 typedef struct s_projectile {
@@ -52,7 +53,7 @@ t_projectile	*tick(t_environment *environment, t_projectile *projectile)
 
 int	exit_hook(int key)
 {
-	if (key == 'a')
+	if (key == 'a' || key == 'q')
 		exit(0);
 	return (1);
 }
@@ -123,53 +124,54 @@ int	init_mlx_image(t_image *image, int width, int height, t_data *data)
 	return (1);
 }
 
+void	draw_clock(t_data *data)
+{
+	t_tuple	*point;
+	t_color	*color;
+	t_matrix *rot;
+	t_tuple *tmp;
+
+	rot = rotation_z(M_PI_2 / 3);
+	point = new_point(0, 1, 0);
+
+	color = new_color(0.6, 0.3, 0.1);
+	for (int i = 0; i < 12; i++)
+	{
+		int x, y;
+
+		x = (point->x) * 150 + WIN_WIDTH / 2.;
+		y = (point->y) * 150 + WIN_HEIGHT / 2.;
+		if (x >= WIN_WIDTH)
+			x = WIN_WIDTH - 1;
+		else if (x < 0)
+			x = 0;
+		if (y >= WIN_HEIGHT)
+			y = WIN_HEIGHT - 1;
+		else if (y < 0)
+			y = 0;
+
+		int size = 4;
+		for (int i = x- size; i < x +  size; i++)
+			for (int j = y- size; j < y +  size; j++)
+				put_pixel(&data->canvas, color, i, j);
+
+		tmp = point;
+		point = matrix_multiply_tuple(rot, tmp);
+		free(tmp);
+	}
+}
+
 int	main(void)
 {
-	// t_data data;
+	t_data data;
 
-	// data.mlx = mlx_init();
-	// data.window = mlx_new_window(data.mlx, WIN_WIDTH, WIN_HEIGHT, "Mini Ray Tracer");
-	// init_mlx_image(&data.canvas, WIN_WIDTH, WIN_HEIGHT, &data);
+	data.mlx = mlx_init();
+	data.window = mlx_new_window(data.mlx, WIN_WIDTH, WIN_HEIGHT, "Mini Ray Tracer");
+	init_mlx_image(&data.canvas, WIN_WIDTH, WIN_HEIGHT, &data);
 	// test_projectile(&data);
-	// mlx_put_image_to_window(data.mlx, data.window, data.canvas.ptr, 0, 0);
-	// mlx_hook(data.window, 2, 1, exit_hook, &data);
-	// mlx_loop(data.mlx);
-	t_matrix *id_matrix = identity_matrix();
-	t_matrix *inv = inverse(id_matrix);
-	t_matrix *a = new_matrix(4, (float[4][4]){
-			{  9 ,  3 ,  0 ,  9 },
-			{ -5 , -2 , -6 , -3 },
-			{ -4 ,  9 ,  6 ,  4 },
-			{ -7 ,  6 ,  6 ,  2 },
-			});
-	printf("inverse of identity matrix:\n");
-	print_matrix(inv);
-	printf("\n");
-	printf("simple matrix:\n");
-	print_matrix(a);
-	printf("\n");
-	t_matrix *inv_a = inverse(a);
-	printf("inverse of the simple matrix:\n");
-	print_matrix(inv_a);
-	t_matrix *a_mult_inv = matrix_multiply(a, inv_a);
-	printf("\n");
-	printf("simple matrix multiplied by its inverse\n");
-	print_matrix(a_mult_inv);
-	printf("\n");
-	printf("transpose of the simple matrix\n");
-	t_matrix *tran = transpose(a);
-	print_matrix(tran);
-	printf("\n");
-	printf("inverse of the transpose of the simple matrix\n");
-	t_matrix *inv_tran = inverse(tran);
-	print_matrix(inv_tran);
-	printf("transpose of the inverse of the simple matrix\n");
-	t_matrix *tran_inv = transpose(inv_a);
-	print_matrix(tran_inv);
-
-	printf("identity matrix vs tuple\n");
-	t_tuple *tuple = new_tuple(1, 2, 3, 1);
-	id_matrix->data[1][2] = 42;
-	t_tuple *tmult = matrix_multiply_tuple(id_matrix, tuple);
-	print_tuple(tmult);
+	draw_clock(&data);
+	mlx_put_image_to_window(data.mlx, data.window, data.canvas.ptr, 0, 0);
+	mlx_hook(data.window, 2, 1, exit_hook, &data);
+	mlx_loop(data.mlx);
+	return (0);
 }
